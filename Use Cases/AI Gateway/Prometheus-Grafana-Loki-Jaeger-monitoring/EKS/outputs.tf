4eb34c4e22fa1b8468fe87de0ea1c7d247989664
vtobi <97698702+vtobi@users.noreply.github.com>
2025-05-19 15:29:34 -0700

    Update outputs.tf

--- Use Cases/AI Gateway/Prometheus-Grafana-Loki-Jaeger-monitoring/EKS/outputs.tf
+++ Use Cases/AI Gateway/Prometheus-Grafana-Loki-Jaeger-monitoring/EKS/outputs.tf
@@ -8,11 +8,6 @@ output "configure_kubectl_local" {
   value       = "aws eks --region ${local.region} update-kubeconfig --name ${module.eks.cluster_name} --kubeconfig ./kubeconfig"
 }
 
-output "elastic_credentials" {
-  description = "For Kibana/Elasticsearch credentials, see elasticsearch-master-credentials secret under elastic namespace, keys: username, password (base64 decode)"
-  value       = "For Kibana/Elasticsearch credentials, see elasticsearch-master-credentials secret under elastic namespace, keys: username, password (base64 decode)"
-}
-
 output "grafana_credentials" {
   description = "For Grafana credentials, see grafana secret under prometheus namespace, keys: admin-user, admin-password (base64 decode)"
   value       = "For Grafana credentials, see grafana secret under prometheus namespace, keys: admin-user, admin-password (base64 decode)"
